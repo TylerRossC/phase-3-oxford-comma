560dbd4b43c434315e1a5df573654f312d6921ee
tyler <tylercharawell@gmail.com>
2021-06-25 10:55:17 -0700

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+    # if array.length === 1
+    #     array.join()
+    # elsif array.length >= 2
+    #     # array.join(" and ")
+    #     array.insert(-2, " and ")
+    # end
+    # array.join(", ")
+    if array.length == 1
+        array.join()
+    elsif array.length == 2
+        array.join(" and ")
+    else 
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
 end
